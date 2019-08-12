88fb97655d09de00eb25cdb57362ca7d6551a9f3
Vasiliy Antufev <vasiliyantufev@gmail.com>
2019-08-12 22:00:04 +0300

    test ssh

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Page Analyzer - @yield('title')</title></head>
 <body>
+
 @section('sidebar')
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Navbar</a>
