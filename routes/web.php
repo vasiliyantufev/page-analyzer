eaf9edca4a47937a0e62473443bf1effcba345c8
Vasiliy Antufev <vasiliyantufev@gmail.com>
2019-08-13 17:08:27 +0300

    test db

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,22 @@
 |
 */
 
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
 $router->get('/', function () use ($router) {
     //return $router->app->version();
     return view('app');
 });
 
 $router->post('/domains', function() {
-    return 'hi';
+    $domains = DB::table('domains')->get();
+
+    $timestamps = Carbon::now();
+    DB::table('domains')->insert(['name' => 'vasy', 'created_at' => $timestamps, 'updated_at' => $timestamps]);
+
+    dd($domains);
+//    return 'hi';
 });
 
 $router->get('/key', function() {
